de33360a54f53e11f08aab0b594cdfb247d195e2
Shubham Ramesh Patil <shubhu244248@gmail.com>
2023-12-13 20:53:36 +0530

    Setter Injection

---
+++ IOCSpringProj01_SetterInjection/src/main/java/com/test/beans/WishMessageGenerator.java
@@ -0,0 +1,35 @@
+package com.test.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+
+//	HAS-A property (Supporting Composition)
+	private Date date;
+	
+	public WishMessageGenerator() {
+		System.out.println("0:Param constructor");
+	}
+	
+	// setter method for Setter injetioon
+	public void setDate(Date date) {
+		System.out.println("Set Date");
+		this.date = date;
+	}
+
+	// msg generator method
+	public String generateMessage(String user) {
+		System.out.println("Generate Wish Messages ");
+		int hr = date.getHours();
+
+		if (hr < 12) {
+			return "Good Morning " + user;
+		} else if (hr < 16) {
+			return "Good Afternoon " + user;
+		} else if (hr < 20) {
+			return "Good Evening " + user;
+		} else {
+			return "Good Night " + user;
+		}
+	}
+}
