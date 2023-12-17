d64caf697c2bee1de3888d54ac06f0735a79674a
Shubham Ramesh Patil <shubhu244248@gmail.com>
2023-12-17 11:29:12 +0530

    Factory Design Pattern With Core Java having Object creation at the End User is aware to create object

---
+++ IOCSpringProje04_FactoryPatter_ProblemInJava/src/main/java/com/spring/comp/Car.java
@@ -0,0 +1,5 @@
+package com.spring.comp;
+
+public abstract class Car {
+	public abstract void drive();
+}
---
+++ IOCSpringProje04_FactoryPatter_ProblemInJava/src/main/java/com/spring/test/BudgetUser.java
@@ -0,0 +1,14 @@
+package com.spring.test;
+
+import com.spring.comp.BudgetCar;
+import com.spring.comp.Car;
+
+public class BudgetUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new BudgetCar("MH 18 JJ 1213 ");
+		car.drive();
+	}
+
+}
