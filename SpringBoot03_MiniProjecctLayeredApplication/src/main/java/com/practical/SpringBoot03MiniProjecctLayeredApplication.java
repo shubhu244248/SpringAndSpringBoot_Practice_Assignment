6e2be854f437dd1acf92335dfe77d492ae4196da
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-10 10:01:52 +0530

    Project Initial Commit

---
+++ SpringBoot03_MiniProjecctLayeredApplication/src/main/java/com/practical/SpringBoot03MiniProjecctLayeredApplication.java
@@ -0,0 +1,13 @@
+package com.practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03MiniProjecctLayeredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot03MiniProjecctLayeredApplication.class, args);
+	}
+
+}
---
+++ SpringBoot03_MiniProjecctLayeredApplication/src/test/java/com/practical/SpringBoot03MiniProjecctLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03MiniProjecctLayeredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
