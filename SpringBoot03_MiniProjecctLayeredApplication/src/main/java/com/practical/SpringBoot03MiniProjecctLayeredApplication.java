9d5fb4f14f506567f95ac23b31e6345129aa4155
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-10 10:14:54 +0530

    Created packages and databse configuration

--- SpringBoot03_MiniProjecctLayeredApplication/src/main/java/com/practical/SpringBoot03MiniProjecctLayeredApplication.java
+++ SpringBoot03_MiniProjecctLayeredApplication/src/main/java/com/practical/SpringBoot03MiniProjecctLayeredApplication.java
@@ -2,12 +2,16 @@ package com.practical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBoot03MiniProjecctLayeredApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBoot03MiniProjecctLayeredApplication.class, args);
+		
+		
 	}
 
 }
