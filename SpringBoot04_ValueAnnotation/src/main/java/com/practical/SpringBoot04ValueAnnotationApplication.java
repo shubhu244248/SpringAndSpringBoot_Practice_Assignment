7232379d59c4c2571d83953c9a29e5a1cae3d6dc
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-16 11:46:50 +0530

    Application in running and value anotation is used

--- SpringBoot04_ValueAnnotation/src/main/java/com/practical/SpringBoot04ValueAnnotationApplication.java
+++ SpringBoot04_ValueAnnotation/src/main/java/com/practical/SpringBoot04ValueAnnotationApplication.java
@@ -2,12 +2,29 @@ package com.practical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.practical.beans.Hospital;
 
 @SpringBootApplication
 public class SpringBoot04ValueAnnotationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot04ValueAnnotationApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringBoot04ValueAnnotationApplication.class, args);
+
+		Hospital hospital = ctx.getBean("hsptl", Hospital.class);
+
+		System.out.println("Spring Bean class object data");
+		System.out.println(hospital);
+
+		System.out.println("System Prperties");
+		System.out.println(System.getProperties());
+
+		System.out.println();
+		
+		((ConfigurableApplicationContext) ctx).close();
+
 	}
 
 }
