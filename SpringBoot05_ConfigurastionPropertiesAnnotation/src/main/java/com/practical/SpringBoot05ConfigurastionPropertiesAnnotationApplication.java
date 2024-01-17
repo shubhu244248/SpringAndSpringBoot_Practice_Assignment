a612ce2aa778b08daf751b9ea975d5270d91ac87
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-17 09:21:03 +0530

    Initial commit

---
+++ SpringBoot05_ConfigurastionPropertiesAnnotation/src/main/java/com/practical/SpringBoot05ConfigurastionPropertiesAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot05ConfigurastionPropertiesAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot05ConfigurastionPropertiesAnnotationApplication.class, args);
+	}
+
+}
