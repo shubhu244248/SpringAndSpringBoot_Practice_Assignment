706c37f624cd004b055616ef1609e747a3d2cfce
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-17 10:14:30 +0530

    Used @Value in ConfigurationProperties in same and the conclusion is @ConfigurationProp having the priority

--- SpringBoot05_ConfigurastionPropertiesAnnotation/src/main/java/com/practical/beans/Employee.java
+++ SpringBoot05_ConfigurastionPropertiesAnnotation/src/main/java/com/practical/beans/Employee.java
@@ -1,16 +1,35 @@
 package com.practical.beans;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
+import lombok.val;
 
 @Component("emp")
 @ConfigurationProperties(prefix = "emp.details")
-@Data
 public class Employee {
 	
+	@Value("${emp.info.name}")
 	private String name;
 	private int age;
+	
+	public Employee() {
+		System.out.println("Employee.0-Paramt - Employee()");
+	}
+
+	public void setName(String name) {
+		System.out.println(this.name);
+		this.name = name;
+		System.out.println("Employee Name : "+ name);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + ", toString()=" + super.toString() + "]";
+	}
+	
+	
 	
 }
